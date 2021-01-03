8cbbf44bd811d989c1480a0a1fec057c5d00c1cf
minhazur9 <minhazur9@gmail.com>
2021-01-03 02:22:42 -0500

    Can now no multiple iterations of the same sample and print the results

--- sim.py
+++ sim.py
@@ -2,31 +2,46 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-def simulate(rate,iterations=1000,y1label='Hits',y2label='Misses'):
-    if(iterations % 2 != 0):
-        iterations += 1
-    x = list(range(1,iterations+1))
-    y = generateNumbers(iterations)
-    generatePlot(x,y,rate,y1label,y2label)
+def simulate(rate,attempts,iterations=1000, y1label='Hits',y2label='Misses'):
+    if(attempts % 2 != 0):
+        attempts += 1
+    for i in range(0,iterations):  
+        population = list(range(1,attempts+1))
+        successes = generateNumbers(attempts)  
+        sampleResults(population,successes,rate)
+    # generatePlot(x,y,rate,y1label,y2label)
 
-def generateNumbers(iterations):
-    y = np.random.random(iterations)
+def generateNumbers(attempts):
+    y = np.random.random(attempts)
     return y
 
-def generatePlot(x,y,rate,y1label,y2label):
+def sampleResults(population,successes,rate):
     size = 50
     rate = rate / 100
-    multiplier = int(len(x)/2)
-    outcomes = []
+    multiplier = int(len(population)/2)
     hits = 0
     misses = 0
-    for outcome in y:
-        if outcome <= 0.047:
-            outcomes.append('hit')
+    for outcome in successes:
+        if outcome <= rate:
             hits += 1
         else:
-            outcomes.append('miss')
             misses += 1
+    print(f'hits:{hits}')
+
+def generatePlot(x,y,rate,y1label,y2label):
+    # size = 50
+    # rate = rate / 100
+    # multiplier = int(len(x)/2)
+    # outcomes = []
+    # hits = 0
+    # misses = 0
+    # for outcome in y:
+    #     if outcome <= rate:
+    #         outcomes.append('hit')
+    #         hits += 1
+    #     else:
+    #         outcomes.append('miss')
+    #         misses += 1
     df = pd.DataFrame(dict(x=x, y=y, label=outcomes))
     groups = df.groupby('label')
     colors = {'hit':'#2AD449', 'miss':'#FF4D4D'}
@@ -38,4 +53,6 @@ def generatePlot(x,y,rate,y1label,y2label):
     ax.legend([f'{y1label} - {hits}',f'{y2label} - {misses}'])
     plt.show()
 
-simulate(4.7,262,'Enderpearls')
+simulate(4.7,262,y1label='Enderpearls')
+
+# simulate(50,305,'Blazerods')
